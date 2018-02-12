2fe2876451e9372e183e80a8ba2e87ed1c647c1a
Pete <32600680+PitGarin@users.noreply.github.com>
2018-02-12 22:11:52 +0300

    no message

---
+++ src/com/home/notes/common/ReadCharRunner.java
@@ -0,0 +1,17 @@
+package com.home.notes.common;
+
+public class ReadCharRunner {
+
+	public static void main(String[] args) {
+
+		int x;
+		try {
+			x = System.in.read();
+			char c = (char) x;
+			System.out.println("Код символа: " + c + " = " + x);
+		} catch (java.io.IOException e) {
+			System.err.println("ошибка ввода " + e);
+
+		}
+	}
+}
